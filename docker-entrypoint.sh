90bd7ba7c44318a6d1420a191217efc71b0110d7
Ravi Sharma <ping2ravi@gmail.com>
2017-05-05 13:26:00 +0100

    Added support for statsd metrics collection

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,8 @@ JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
 JOB_MANAGER_RPC_PORT=${JOB_MANAGER_RPC_PORT:-6123}
 JOB_MANAGER_WEB_PORT=${JOB_MANAGER_WEB_PORT:-8081}
 JOB_MANAGER_HEAP_MB=${JOB_MANAGER_HEAP_MB:-1025}
+STATSD_SERVER=${STATSD_SERVER:-localhost}
+STATSD_PORT=${STATSD_PORT:-8125}
 JOB_MANAGER_WEB_SUBMIT_ENABLED=${JOB_MANAGER_WEB_SUBMIT_ENABLED:-true}
 TASK_MANAGER_TASK_SLOTS=${TASK_MANAGER_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
 TASK_MANAGER_HEAP_MB=${TASK_MANAGER_HEAP_MB:-2049}
@@ -16,10 +18,13 @@ echo JOB_MANAGER_HEAP_MB=$JOB_MANAGER_HEAP_MB
 echo JOB_MANAGER_WEB_SUBMIT_ENABLED=$JOB_MANAGER_WEB_SUBMIT_ENABLED
 echo TASK_MANAGER_TASK_SLOTS=$TASK_MANAGER_TASK_SLOTS
 echo TASK_MANAGER_HEAP_MB=$TASK_MANAGER_HEAP_MB
+echo ENABLE_STATSD_METRICS=$ENABLE_STATSD_METRICS
 echo Running : $1
 
 ls -lrt  /etc/init.d/
 /etc/init.d/ssh start
+
+
 
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
     echo "Usage: $(basename $0) (jobmanager|taskmanager)"
@@ -31,7 +36,7 @@ elif [ "$1" = "jobmanager" ]; then
     sed -i -e "s/jobmanager.web.port: 8081/jobmanager.web.port: ${JOB_MANAGER_WEB_PORT}/g" $FLINK_HOME/conf/flink-conf.yaml
     sed -i -e "s/jobmanager.heap.mb: 256/jobmanager.heap.mb: ${JOB_MANAGER_HEAP_MB}/g" $FLINK_HOME/conf/flink-conf.yaml
     sed -i -e "s/#jobmanager.web.submit.enable: false/jobmanager.web.submit.enable: ${JOB_MANAGER_WEB_SUBMIT_ENABLED}/g" $FLINK_HOME/conf/flink-conf.yaml
-
+   
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
     exec $FLINK_HOME/bin/jobmanager.sh start-foreground cluster
 elif [ "$1" = "taskmanager" ]; then
@@ -39,7 +44,15 @@ elif [ "$1" = "taskmanager" ]; then
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
     sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: ${TASK_MANAGER_TASK_SLOTS}/g" $FLINK_HOME/conf/flink-conf.yaml
     sed -i -e "s/taskmanager.heap.mb: 512/taskmanager.heap.mb: ${TASK_MANAGER_HEAP_MB}/g" $FLINK_HOME/conf/flink-conf.yaml
+    if [ "$ENABLE_STATSD_METRICS" = true ]; then
+       echo 'Replacing Content'
+       sed -i -e "s/#metrics.reporters:/metrics.reporters: statsd/g" $FLINK_HOME/conf/flink-conf.yaml
+       sed -i -e "s/#metrics.reporter.stsd.class: org.apache.flink.metrics.statsd.StatsDReporter/metrics.reporter.stsd.class: org.apache.flink.metrics.statsd.StatsDReporter/g" $FLINK_HOME/conf/flink-conf.yaml
+       sed -i -e "s/#metrics.reporter.stsd.host: localhost/metrics.reporter.stsd.host: ${STATSD_SERVER}/g" $FLINK_HOME/conf/flink-conf.yaml
+       sed -i -e "s/#metrics.reporter.stsd.port: 8125/metrics.reporter.stsd.port: ${STATSD_PORT}/g" $FLINK_HOME/conf/flink-conf.yaml
 
+
+    fi
     echo "Starting Task Manager"
     echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
     exec $FLINK_HOME/bin/taskmanager.sh start-foreground
